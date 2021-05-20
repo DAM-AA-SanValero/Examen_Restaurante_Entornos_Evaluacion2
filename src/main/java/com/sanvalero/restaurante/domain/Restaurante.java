3634c96d799926e56d3f38378fae572fb142a53b
Joserra2304 <Joserra2304@gmail.com>
2021-05-20 19:53:51 +0200

    Añadida clase Restaurante

---
+++ src/main/java/com/sanvalero/restaurante/domain/Restaurante.java
@@ -0,0 +1,52 @@
+package com.sanvalero.restaurante.domain;
+
+import java.util.Date;
+
+public class Restaurante {
+
+    private String nombre;
+    private String descripcion;
+    private String tipo;
+    private String direccion;
+    private Date horario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+}
